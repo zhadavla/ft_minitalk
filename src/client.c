a40a662eda630ff81de614aba58cda73e08cf028
Vladyslav Zhadan <zhadavla@fit.cvut.cz>
2023-09-12 19:14:51 +0200

    finished?

--- src/client.c
+++ src/client.c
@@ -2,18 +2,55 @@
 #include <string.h>
 
 
+/**
+ * Sends a char to a process
+ * meaning it sends 8 signals to the process
+ * representing the 8 bits of the char
+ * it iterates through the bits of the char
+ * and sends a SIGUSR1 to the process if the bit is 1  else it sends SIGUSR2
+*/
+void send_char(int pid, char c)
+{
+	int i;
 
-int main(int agrc, char **argv)
+	i = 0;
+	while (i < 8)
+	{
+		usleep(1);
+		if (c & 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		c = c >> 1;
+		usleep(500);
+		i++;
+	}
+}
+
+void send_string(int pid, char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		send_char(pid, str[i]);
+		i++;
+	}
+}
+
+int main(int argc, char **argv)
 {
 	int pid;
 	int i;
 	int j;
 
-
+	if (argc != 3)
+	{
+		printf("Usage: ./client [pid] [string]\n");
+		return 1;
+	}
 	pid = atoi(argv[1]);
-	if (atoi(argv[2]) % 2 == 0)
-		kill(pid, SIGUSR1);
-	else 
-		kill(pid, SIGUSR2);
+	send_string(pid, argv[2]);
 	return 0;
 }
--- src/server.c
+++ src/server.c
@@ -1,13 +1,49 @@
 #include "../ft_minitalk.h"
 
+int current_bit = 0;
+
+void print_char(void){
+	// printf("current bit = {%d}\n", current_bit);
+	write(1, &current_bit, 1);
+}
+
+// may be rewriteten as banch of if statements
+static int ft_pow_of_two(int exp){
+	if (exp == 0)
+		return 1;
+	if (exp == 1)
+		return 2;
+	if (exp == 2)
+		return 4;
+	if (exp == 3)
+		return 8;
+	if (exp == 4)	
+		return 16;
+	if (exp == 5)	
+		return 32;
+	if (exp == 6)
+		return 64;
+	if (exp == 7)
+		return 128;
+}
 
 void handler(int signum){
+	static int counter = 0;
+
 	if (signum == SIGUSR1)
-		printf("SIGUSR1\n");
+	{	
+		current_bit += ft_pow_of_two(counter);
+		counter++;
+	}
 	else if (signum == SIGUSR2)
-		printf("SIGUSR2\n");
-	else
-		printf("Error\n");
+		counter++;
+	if (counter == 8){
+		counter = 0;
+		print_char();
+		current_bit = 0;
+	}
+	usleep(5);
+	
 }
 
 
@@ -23,6 +59,6 @@ int main(void){
 	pid = getpid();
 	printf("pid = %d\n", pid);
 	while (1)
-		pause();
+		sleep(1);
 	return 0;
 }
